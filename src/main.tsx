daaa11c53075bebb476f729db6229a27803d1b40
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-28 14:51:52 -0300

    feat: configuring the base of the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom'
+import { App } from './App.tsx'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.tsx"
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        'cardShadow': '0 7px 29px 0px rgba(100, 100, 111, 0.2)',
+      }
+    },
+  },
+  plugins: [],
+}
+
