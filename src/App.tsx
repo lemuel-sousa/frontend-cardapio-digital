f093400a0ff6bf4d4572e9592ba179c16e243585
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-31 14:49:55 -0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import './App.css';
+import { useState } from 'react';
 import { Card } from './components/Card';
+import { CreateModal } from './components/CreateModal';
 import './global.css';
-import { FoodData } from './interface/FoodData';
+import { useFoodData } from './hooks/useFoodData';
 
 export function App() {
 
-  const data: FoodData[] = [];
+  const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const hendleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
-
-    <div className="">
-      <h1 className="">Cardápio</h1>
-
-      <div className="grid">
-        {data.map(foodData =>
+    <div className="mt-8 flex flex-col items-center justify-center gap-6 mb-20">
+      <h1 className="text-3xl font-semibold font-inter">Cardápio Digital</h1>
+      <div className="grid grid-cols-3 gap-8 ">
+        {data?.map((foodData, i) =>
           <Card
             image={foodData.image}
             title={foodData.title}
             price={foodData.price}
+            key={i}
           />
         )}
       </div>
+      {isModalOpen && <CreateModal closeModal={hendleOpenModal} />}
+      <button
+        className="fixed left-5 top-[50%] px-6 py-4 bg-cyan-600 hover:bg-cyan-700 rounded-lg text-white font-semibold font-lato"
+        onClick={hendleOpenModal}>
+        novo
+      </button>
     </div>
 
   )
