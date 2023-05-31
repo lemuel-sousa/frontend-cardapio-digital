3efcb15c9b6d57374a0efa13751fe2b5fc2bbdb4
lemuel-sousa <lemuel124.ldsp@gmail.com>
2023-05-31 14:45:28 -0300

    feat: add item card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,19 @@
 interface CardProps {
     image: string,
     title: string,
-    price: number,
+    price: number
 }
 
+
 export function Card({ image, title, price }: CardProps) {
+
     return (
-        <div className="flex flex-col items-center justify-center w-60 rounded-lg shadow-cardShadow">
+        <div className="flex flex-col items-center justify-between w-64 h-96 pb-4 rounded-lg shadow-cardShadow">
             <img
-                className="rounded-tr-lg"
+                className=" rounded-lg rounded-tr-lg "
                 src={image} alt="" />
-            <h2>{title}</h2>
-            <p className="w-60 h-48"><b>Preço: {price}</b></p>
+            <h2 className="font-semibold font-sans">{title}</h2>
+            <p><b>Preço:</b>{price}</p>
 
         </div>
     )
